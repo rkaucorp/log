b85bcee35ce9da0e1e53a60c4ed53b0ec521fd84
rkaucorp <rezaul.karim@springrain.io>
2022-05-09 10:04:00 +0600

    change success status code

--- src/config/variable.ts
+++ src/config/variable.ts
@@ -6,7 +6,7 @@ export const AWS_DYNAMODB_END_POINT = process.env.AWS_END_POINT;
 export const AWS_DYNAMODB_TABLE_NAME = process.env.DYNAMO_DB_TABLE;
 export const AWS_DYNAMODB_TABLE_NAME_FOR_LOG =
   process.env.DYNAMO_DB_TABLE_FOR_LOG;
-export const SUCCESS_STATUS_CODE = 202;
+export const SUCCESS_STATUS_CODE = 200;
 export const FAILED_STATUS_CODE = 503;
 export const UN_AUTHORIZED_STATUS_CODE = 401;
 export const EMPTY_BODY_STATUS_CODE = 403;
